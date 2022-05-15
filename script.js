66f572b161d88f1f4ec743b0035943daa51b5ea5
writecards <rw2423@nyu.edu>
2022-05-14 23:48:18 -0700

    title add

--- script.js
+++ script.js
@@ -9,3 +9,35 @@ let box4 = document.getElementById("box4");
 //     console.log("intro clicked")
 //     location.href = "intro/index.html"
 // })
+
+box1.addEventListener("mouseenter", ()=>{
+    document.getElementById("pagetitle1").innerHTML = "the algorithmic beauty of slow violence"
+})
+
+box1.addEventListener("mouseleave",()=>{
+    document.getElementById("pagetitle1").innerHTML = "1";
+})
+
+box2.addEventListener("mouseenter", ()=>{
+    document.getElementById("pagetitle2").innerHTML = "understanding our interconnected world"
+})
+
+box2.addEventListener("mouseleave",()=>{
+    document.getElementById("pagetitle2").innerHTML = "2";
+})
+
+box3.addEventListener("mouseenter", ()=>{
+    document.getElementById("pagetitle3").innerHTML = "corporate rhetoric as reassurance"
+})
+
+box3.addEventListener("mouseleave",()=>{
+    document.getElementById("pagetitle3").innerHTML = "3";
+})
+
+box4.addEventListener("mouseenter", ()=>{
+    document.getElementById("pagetitle4").innerHTML = "naming emotions is a start"
+})
+
+box4.addEventListener("mouseleave",()=>{
+    document.getElementById("pagetitle4").innerHTML = "4";
+})
